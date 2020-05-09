92c7af365f6ea51adedc467c54c40cb43e39e058
SITANSHU KUMAR <41771170+sitanshu-kumar-vst-au4@users.noreply.github.com>
2020-05-09 14:26:44 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sitanshu kumar
         </a>
       </header>
     </div>
